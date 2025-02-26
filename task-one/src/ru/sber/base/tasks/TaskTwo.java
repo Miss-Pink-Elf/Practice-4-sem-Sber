aa73aaa081df0bcce2b38752c3a022fa648987b0
Ksenia Ilina <ksulishnja@gmail.com>
2025-02-26 20:10:38 +0300

    Создание класса со вторым заданием

---
+++ task-one/src/ru/sber/base/tasks/TaskTwo.java
@@ -0,0 +1,34 @@
+package ru.sber.base.tasks;
+
+import java.util.Scanner;
+
+public class TaskTwo {
+
+    public static void main(String[] args) {
+        TaskTwo task2 = new TaskTwo();
+        task2.exec();
+    }
+
+    public void exec() {
+        /*
+        int age;
+        age = 19;
+
+        System.out.println("Возраст: " + age);
+        */
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите два натуральных числа a и b: ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int res = a / b;
+        int rem = a % b;
+        if (b > 0 || a > 0) {
+            System.out.println(a + " / " + b + " = " + res + " и " + rem + " в остатке");
+        }
+        else {
+            System.out.println("Вы ввели ненатуральное число");
+        }
+
+    }
+}
