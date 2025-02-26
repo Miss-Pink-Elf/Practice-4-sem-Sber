ca0d46c275ae7665050c3ff63b6ac743e9d453bc
Ksenia Ilina <ksulishnja@gmail.com>
2025-02-26 20:42:42 +0300

    Создание классов с восьмым и девятым заданиями. Рефакторинг

---
+++ task-one/src/ru/sber/base/tasks/TaskEight.java
@@ -0,0 +1,35 @@
+package ru.sber.base.tasks;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TaskEight {
+
+    public static void main(String[] args) {
+        TaskEight task8 = new TaskEight();
+        task8.exec();
+    }
+
+    public void exec() {
+        Random random = new Random();
+        char HiddenLetter = (char) (random.nextInt(26) + 'A');
+//        char HiddenLetter = 'R';
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите букву от A до Z: ");
+
+        char ch;
+        while (true) {
+            ch = in.nextLine().toUpperCase().charAt(0);
+
+            if (ch == HiddenLetter) {
+                System.out.println("Right!");
+                break;
+            }
+            else if (ch < HiddenLetter)
+                System.out.println("You're too low");
+            else
+                System.out.println("You're too high");
+        }
+    }
+}
---
+++ task-one/src/ru/sber/base/tasks/TaskNine.java
@@ -0,0 +1,35 @@
+package ru.sber.base.tasks;
+
+import java.util.Scanner;
+
+public class TaskNine {
+
+    public static void main(String[] args) {
+        TaskNine task9 = new TaskNine();
+        task9.exec();
+    }
+
+    public void exec() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите три вещественных числа (коэффициенты квадратного уравнения): ");
+
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        double c = in.nextDouble();
+        double D = b * b - 4 * a * c;
+
+        if (D < 0)
+            System.out.println("У уравнения нет корней");
+        else if (D == 0) {
+            double x = (- b - Math.sqrt(D)) / (2 * a);
+            
+            System.out.println("У уравнения один корень, x = " + x);
+        }
+        else {
+            double x1 = (- b - Math.sqrt(D)) / (2 * a);
+            double x2 = (- b + Math.sqrt(D)) / (2 * a);
+
+            System.out.println("У уравнения два корня, x1 = " + x1 + ", x2 = " + x2);
+        }
+    }
+}
