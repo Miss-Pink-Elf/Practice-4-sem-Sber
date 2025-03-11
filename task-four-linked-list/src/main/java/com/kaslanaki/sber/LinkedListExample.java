47b134d1b74cadc3852590697c7b68d9f554381d
Ksenia Ilina <ksulishnja@gmail.com>
2025-03-11 22:41:30 +0300

    Задание 4. Односвязный список

---
+++ task-four-linked-list/src/main/java/com/kaslanaki/sber/LinkedListExample.java
@@ -0,0 +1,17 @@
+package com.kaslanaki.sber;
+
+public class LinkedListExample {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add(10);
+        list.add(25);
+        list.add(39);
+        list.printList(); // Вывод: 10 -> 25 -> 39 -> null
+
+        list.remove(25);
+        list.printList(); // Вывод: 10 -> 39 -> null
+
+        System.out.println("Размер списка: " + list.size()); // Вывод: Размер списка: 2
+    }
+}
---
+++ task-four-linked-list/src/main/java/com/kaslanaki/sber/SinglyLinkedList.java
@@ -0,0 +1,57 @@
+package com.kaslanaki.sber;
+
+class SinglyLinkedList {
+    Node head;
+    private int size;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void remove(int data) {
+        if (head == null) return;
+        if (head.data == data) {
+            head = head.next;
+            size--;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            if (current.next.data == data) {
+                current.next = current.next.next;
+                size--;
+                return;
+            }
+            current = current.next;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
